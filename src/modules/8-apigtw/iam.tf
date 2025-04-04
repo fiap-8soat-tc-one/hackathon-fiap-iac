5d98295bd8c7437a103e81ea4ccec5e2d337d091
Rodrigo Trancoso <trancosorodrigo1@gmail.com>
2025-04-03 23:00:41 -0300

    feat: fix api gateway resource

---
+++ src/modules/8-apigtw/iam.tf
@@ -0,0 +1,69 @@
+resource "aws_iam_role" "apigateway_invoke_lambda_role" {
+  name = "apigateway-invoke-lambda-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Effect = "Allow",
+      Principal = {
+        Service = "apigateway.amazonaws.com"
+      },
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
+
+resource "aws_iam_role_policy" "invoke_lambda_policy" {
+  name = "invoke-lambda-from-apigateway"
+  role = aws_iam_role.apigateway_invoke_lambda_role.id
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Action = [
+          "lambda:InvokeFunction"
+        ],
+        Resource = [
+          "${var.auth_lambda_arn}",
+          "${var.presigned_lambda_arn}"
+        ]
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role" "apigateway_dynamodb_role" {
+  name = "apigateway-dynamodb-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Action = "sts:AssumeRole",
+      Effect = "Allow",
+      Principal = {
+        Service = "apigateway.amazonaws.com"
+      }
+    }]
+  })
+}
+
+resource "aws_iam_role_policy" "apigateway_policy" {
+  name = "dynamodb-get-policy"
+  role = aws_iam_role.apigateway_dynamodb_role.id
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Action = [
+          "dynamodb:GetItem",
+        ],
+        Effect   = "Allow",
+        Resource = "${var.dynamodb_table_arn}"
+      }
+    ]
+  })
+}
+
--- src/modules/8-apigtw/variable.tf
+++ src/modules/8-apigtw/variable.tf
@@ -1,34 +1,39 @@
 variable "aws_api_gateway_authorizer_name" {
-  type        = string
-  default     = "cognito-authorizer"
+  type    = string
+  default = "cognito-authorizer"
 }
 
 variable "aws_api_gateway_authorizer_type" {
-  type        = string
-  default     = "COGNITO_USER_POOLS"
+  type    = string
+  default = "COGNITO_USER_POOLS"
 }
 
 variable "aws_api_gateway_rest_api_name" {
-  type        = string
-  default     = "fiap-hackaton-api"
+  type    = string
+  default = "fiap-hackaton-api"
+}
+
+variable "stage_name" {
+  type    = string
+  default = "dev"
 }
 
 variable "presigned_lambda_arn" {
-  type        = string
+  type = string
 }
 
 variable "auth_lambda_arn" {
-  type        = string
+  type = string
 }
 
 variable "user_pool_id" {
-  type        = string
+  type = string
 }
 
 variable "dynamodb_table_name" {
-  type        = string
+  type = string
 }
 
 variable "dynamodb_table_arn" {
-  type        = string
+  type = string
 }
